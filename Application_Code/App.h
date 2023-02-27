2f98c117627bbc58c87479728776b651b710405a
Mustafa-Montaser <mustafamontaser13@gmail.com>
2023-02-27 13:04:55 +0200

    update

--- Application_Code/App.h
+++ Application_Code/App.h
@@ -36,6 +36,7 @@ void App_SystemInit(void);
 /**
  * @brief Set ON/OFF button handler
  * @note If not used, application will use default ON/OFF button handler
+ * @note If used, must be called before system initialise
  * @param onOffBtnCallBack Pointer to function takes void and return void
  * @return void
  * */
@@ -46,6 +47,7 @@ void App_SetOnOffBtnHandler(void (*onOffBtnCallBack) (void));
 /**
  * @brief Set UP button handler
  * @note If not used, application will use default UP button handler
+ * @note If used, must be called before system initialise
  * @param upBtnCallBack Pointer to function takes void and return void
  * @return void
  * */
@@ -56,6 +58,7 @@ void App_SetUpBtnHandler(void (*upBtnCallBack) (void));
 /**
  * @brief Set DOWN button handler
  * @note If not used, application will use default DOWN button handler
+ * @note If used, must be called before system initialise
  * @param downBtnCallBack Pointer to function takes void and return void
  * @return void
  * */
@@ -66,6 +69,7 @@ void App_SetDownBtnHandler(void (*downBtnCallBack) (void));
 /**
  * @brief Set timer handler
  * @note If not used, application will use default timer handler
+ * @note If used, must be called before system initialise
  * @param timerCallBack Pointer to function takes void and return void
  * @return void
  * */
